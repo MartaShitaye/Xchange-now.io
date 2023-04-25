e73de69fac8b83a0a7c1ee8f3f8717699b76ecb0
MartaShitaye <90278932+MartaShitaye@users.noreply.github.com>
2023-04-25 04:52:51 -0700

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,27 @@
+<div class="chat-container">
+  <div class="chat-header">
+    <h3>Chatting with John Doe</h3>
+  </div>
+  <div class="chat-messages">
+    <div class="message received">
+      <p>Hi, I'm interested in your item. Can you tell me more about it?</p>
+      <span class="timestamp">9:30am</span>
+    </div>
+    <div class="message sent">
+      <p>Sure, it's in great condition and works perfectly.</p>
+      <span class="timestamp">9:35am</span>
+    </div>
+    <div class="message received">
+      <p>Would you be willing to take $50 for it?</p>
+      <span class="timestamp">9:40am</span>
+    </div>
+    <div class="message sent">
+      <p>Sorry, I'm really looking for at least $75.</p>
+      <span class="timestamp">9:45am</span>
+    </div>
+  </div>
+  <div class="chat-input">
+    <input type="text" placeholder="Type a message...">
+    <button>Send</button>
+  </div>
+</div>
